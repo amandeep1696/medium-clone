43b50a8348278caea35d79f5819cdff629a22c0d
Amandeep Singh <am418470@dal.ca>
2024-05-23 16:38:10 -0300

    Modify middlewares to support Context variables

--- backend/src/middleware/jwtMiddleware.ts
+++ backend/src/middleware/jwtMiddleware.ts
@@ -1,28 +1,32 @@
 import { Next, Context } from 'hono';
 import { verify } from 'hono/jwt';
 import { ContextBindings, ContextVariables } from '../types/context';
+import { createMiddleware } from 'hono/factory';
+import { Variables } from 'hono/types';
 
-export const jwtMiddleware = async (c: Context, next: Next) => {
-  const authHeader = c.req.header('authorization');
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    c.status(401);
-    return c.json({
-      error: 'Authorization header missing or malformed',
-    });
-  }
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const payload = await verify(token, c.env.JWT_SECRET);
-    if (!payload || !payload.id) {
-      throw new Error('Invalid token payload');
+export const jwtMiddleware = createMiddleware<{ Variables: ContextVariables }>(
+  async (c: Context, next: Next) => {
+    const authHeader = c.req.header('authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      c.status(401);
+      return c.json({
+        error: 'Authorization header missing or malformed',
+      });
     }
-    c.set('userId', payload.id);
-    await next();
-  } catch (e) {
-    c.status(401);
-    return c.json({
-      error: 'Unauthorized User',
-    });
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const payload = await verify(token, c.env.JWT_SECRET);
+      if (!payload || !payload.id) {
+        throw new Error('Invalid token payload');
+      }
+      c.set('userId', payload.id);
+      await next();
+    } catch (e) {
+      c.status(401);
+      return c.json({
+        error: 'Unauthorized User',
+      });
+    }
   }
-};
+);
--- backend/src/middleware/prismaMiddleware.ts
+++ backend/src/middleware/prismaMiddleware.ts
@@ -1,11 +1,15 @@
 import { Context, Next } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { createMiddleware } from 'hono/factory';
+import { ContextVariables } from '../types/context';
 
-export const prismaMiddleware = async (c: Context, next: Next) => {
+export const prismaMiddleware = createMiddleware<{
+  Variables: ContextVariables;
+}>(async (c: Context, next: Next) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
   c.set('prisma', prisma);
   await next();
-};
+});
