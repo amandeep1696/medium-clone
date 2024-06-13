b419a95580115212edb077f93980e957a97df3c7
Amandeep Singh <am418470@dal.ca>
2024-06-13 15:32:13 -0300

    add cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,17 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import userRouter from './routes/user';
 import blogRouter from './routes/blog';
 import { ContextBindings, ContextVariables } from './types/context';
+
+// todo deploy it
 
 const app = new Hono<{
   Bindings: ContextBindings;
   Variables: ContextVariables;
 }>();
+
+app.use('/*', cors());
 
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog', blogRouter);
